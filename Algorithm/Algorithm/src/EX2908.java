98296840999922abe7aabae8aade47c2d3a4dd8e
leegyeseung <lgs0004237@naver.com>
2024-09-08 14:33:02 +0900

    test0908

---
+++ Algorithm/Algorithm/src/EX2908.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class EX2908 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input =  br.readLine();
+        StringTokenizer st = new StringTokenizer(input," ");
+        String num1  = st.nextToken();
+        String num2  = st.nextToken();
+
+    }
+
+    public void change(String input) {
+        char temp = input.charAt(0);
+        
+    }
+}
